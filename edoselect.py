2d267d9db3aeeab7c4602606f2826443a2268496
sharkretriever <thermostatico@gmail.com>
2018-12-12 22:18:04 -0500

    Revamped the calculation file completely

--- edoselect.py
+++ edoselect.py
@@ -1,83 +1,81 @@
 from math import log
 
-def find_ratio_squared_error(edo, actual_ratio):
-    cur_error = (1200 * log(1 / actual_ratio) / log(2)) ** 2
-    for i in range(1, edo + 1):
-        cur_error = min(cur_error, (1200 * log(2 ** (i/edo) / actual_ratio) / log(2)) ** 2)
-    return cur_error
+# Goal:
+# Input: a list of just intonation (JI) ratios
+# Output: a list of EDOs that progressively approximate the given ratios better (absolutely)
+#         For each such EDO, print out the tone numbers (0-indexed) that approximate the ratios in input order
+# Example format: 
+#   Input: [3/2, 5/4]
+#   Output:
+#   12 EDO (3/2: note 7 with error -2.0 cents, 5/4: note 4 with error +13.7 cents)
+#   [some other EDOs, maybe]
+#   58 EDO (3/2: note 34 with error +1.5 cents, 5/4: note 19 with error +6.8 cents)
+#   [some more EDOs]
 
-def find_ratio_error(edo, actual_ratio):
-    cur_error = 1200 * log(1 / actual_ratio) / log(2)
-    for i in range(1, edo + 1):
-        potential_error = 1200 * log(2 ** (i/edo) / actual_ratio) / log(2)
-        if abs(potential_error) < abs(cur_error):
-            cur_error = potential_error
+# EDO-record is of type {'edo': Int, 'approximations': [{'ratio': Rational, 'note_index': Int, 'error': Double}]}
 
-    return cur_error
+# find_approximation_error: Int -> Double -> (Int, Double)
+# returns (note_index, approximation_error_in_cents)
+def find_approximation_error(edo_number, ratio_to_approximate):
+    # hand-derived; we want to find n to approximate 2^(n/N)/R to 1 as close as possible,
+    # where R = ratio_to_approximate, n = edo_number
+    closest_note_index = round(edo_number * log(ratio_to_approximate) / log(2))
+    expected_ratio = 2 ** (closest_note_index / edo_number)
+    # from http://www.sengpielaudio.com/calculator-centsratio.htm
+    approximation_error_in_cents = 1200 * (log(expected_ratio) - log(ratio_to_approximate)) / log(2)
+    return (closest_note_index, approximation_error_in_cents)
 
+# has_better_approximations: EDO-record -> EDO-record -> Boolean
+# returns true if competing_edo_record has better approximations than (or equal approximation to) best_edo_so_far for every given ratio
+def has_better_approximations(best_edo_so_far, competing_edo_record):
+    for best_error, competing_error in zip(best_edo_so_far['approximations'], competing_edo_record['approximations']):
+        if abs(best_error['error']) < abs(competing_error['error']):
+            return False
+    return True
 
-def find_edo_error5(edo):
-    return 4 * find_ratio_squared_error(edo, 6/5) + 9 * find_ratio_squared_error(edo, 5/4) + 16 * find_ratio_squared_error(edo, 3/2)
+# rational_to_float: String -> Double
+# The strings are of the form $n/$d, so fractions.
+def rational_to_float(ratio):
+    numerator = float(ratio.split("/")[0])
+    denominator = float(ratio.split("/")[1])
+    return numerator / denominator
 
-def find_edo_long_error5(edo):
-    return {"perfect_fifth_error": find_ratio_error(edo, 3/2), "major_third_error": find_ratio_error(edo, 5/4), "minor_third_error": find_ratio_error(edo, 6/5)}
+# find_progressively_better_edos: [String] -> [EDO-record]
+# The strings are of the form $n/$d, so fractions.
+def find_progressively_better_edos(ratio_input_list, lowest_edo = 12, highest_edo = 1000):
+    progressively_better_edos = []
+    best_edo_so_far = None
+    for edo in range(lowest_edo, highest_edo + 1):
+        temp_edo_record = {'edo': edo, 'approximations': []}
+        for ratio in ratio_input_list:
+            index, error = find_approximation_error(edo, rational_to_float(ratio))
+            temp_edo_record['approximations'].append({'ratio': ratio, 'note_index': index, 'error': error})
+        if best_edo_so_far is None or has_better_approximations(best_edo_so_far, temp_edo_record):
+            progressively_better_edos.append(temp_edo_record)
+            best_edo_so_far = temp_edo_record
+    return progressively_better_edos
 
-def print_edo_long_error5(edo_long_error):
-    print("3/2: " + str(round(edo_long_error["perfect_fifth_error"], 2)) + ", 5/4: " + str(round(edo_long_error["major_third_error"], 2)) + 
-            ", 6/5: " + str(round(edo_long_error["minor_third_error"], 2)))
-
-
-def find_edo_error7(edo):
-    return find_ratio_squared_error(edo, 7/4) + 4 * find_ratio_squared_error(edo, 6/5) + 9 * find_ratio_squared_error(edo, 5/4) + 16 * find_ratio_squared_error(edo, 3/2)
-
-def find_edo_long_error7(edo):
-    return {"perfect_fifth_error": find_ratio_error(edo, 3/2), "major_third_error": find_ratio_error(edo, 5/4), "minor_third_error": find_ratio_error(edo, 6/5), "natural_seventh_error": find_ratio_error(edo, 7/4)}
-
-def print_edo_long_error7(edo_long_error):
-    print("3/2: " + str(round(edo_long_error["perfect_fifth_error"], 2)) + ", 5/4: " + str(round(edo_long_error["major_third_error"], 2)) + 
-            ", 6/5: " + str(round(edo_long_error["minor_third_error"], 2)) + ", 7/4: " + str(round(edo_long_error["natural_seventh_error"], 2)))
-
-
-def find_beaten_list(min_edo, max_edo, error_func, long_error_func):
-    edo_errors = []
-    for edo in range(min_edo, max_edo + 1):
-        edo_errors.append({"edo": edo, "error": error_func(edo), "long_error": long_error_func(edo)})
-        
-    minimal_pair = edo_errors[0]
-    beaten_list = [minimal_pair]
-
-    for pair in edo_errors:
-        if (pair["error"] < minimal_pair["error"]):
-            minimal_pair = pair
-            beaten_list.append(pair)
-
-    return beaten_list
-
-
-def print_beaten_list(beaten_list, print_long_error_func):
-    if len(beaten_list) == 0:
-        return
-
-    print(str(beaten_list[0]["edo"]) + "-EDO with error " + str(round(beaten_list[0]["error"], 2)))
-    print_long_error_func(beaten_list[0]["long_error"])
-
-    minimal_pair = beaten_list[0]
-    for pair in beaten_list[1:]:
-        if pair["error"] < minimal_pair["error"]:
-            print("Beaten by " + str(pair["edo"]) + "-EDO with error " + str(round(pair["error"], 2)))
-            print_long_error_func(pair["long_error"])
-            minimal_pair = pair
-
+# print_progressively_better_edos: [EDO-record] -> Void (prints to stdout)
+def print_progressively_better_edos(edo_records):
+    # Example format for each record: 
+    # 12 EDO (3/2: note 7 with error -2.0 cents, 5/4: note 4 with error +13.7 cents)
+    for edo_record in edo_records:
+        edo_approximation_strings_list = ["%s: note %d with error %s%0.2f cents" % 
+            (a['ratio'], a['note_index'], '+' if a['error'] >= 0 else '-', abs(a['error']))
+            for a in edo_record["approximations"]]
+        edo_string = "%d EDO (%s)" % (edo_record["edo"], ", ".join(edo_approximation_strings_list))
+        print(edo_string)
 
 def main():
-    print("Good perfect fifths, major thirds, minor thirds")
-    beaten_list5 = find_beaten_list(12, 1000, find_edo_error5, find_edo_long_error5)
-    print_beaten_list(beaten_list5, print_edo_long_error5)
-
-    print()
-    print("Good perfect fifths, major thirds, minor thirds, natural sevenths")
-    beaten_list7 = find_beaten_list(12, 1000, find_edo_error7, find_edo_long_error7)
-    print_beaten_list(beaten_list7, print_edo_long_error7)
+    ratio_input_lists = [["3/2", "5/4", "6/5"], ["3/2", "5/4", "6/5", "9/8"],
+        ["3/2", "5/4", "6/5", "9/8", "7/4"], ["3/2", "5/4", "6/5", "9/8", "7/4", "11/8"],
+        ["3/2", "5/4", "6/5", "9/8", "7/4", "11/8", "7/5"],
+        ["3/2", "5/4", "6/5", "9/8", "7/4", "11/8", "7/5", "7/6", "9/7"],
+        ["3/2", "5/4", "6/5", "9/8", "7/4", "11/8", "7/5", "7/6", "9/7", "11/9"]]
+    for ratio_input_list in ratio_input_lists:
+        progressively_better_edos = find_progressively_better_edos(ratio_input_list, highest_edo = 200)
+        print_progressively_better_edos(progressively_better_edos)
+        print("")
 
 
 if __name__ == "__main__":
